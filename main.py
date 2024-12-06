c619dccf1b2ad9d193faa4cb7161c5ed56649ff4
gibrambila <167575338+gibrambila@users.noreply.github.com>
2024-12-06 16:05:06 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,124 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+import fdb
 
 app = Flask(__name__)
 
-series_list = [
-    {"serie": 1, "titulo": "Heartland", "genero": "Drama/Comédia/família", "ano": "2007", "descricao": "Em meio a segredos, paixões e rivalidades, a família Heartland se mantém unida para enfrentar os desafios da vida e administrar sua fazenda.", "imagem": "Heartland.jfif"},
-    {"serie": 2, "titulo": "Gossip Girl", "genero": "Drama/Romance", "ano": "2007", "descricao": "Explorações de eventos inexplicáveis, romances, moda e enigmas.", "imagem": "gossip_girl.jpg"},
-    {"serie": 3, "titulo": "Mako Mermaids", "genero": "Drama/Romance/ficção científica", "ano": "201", "descricao": "Uma série emocionante sobre uma jovem que enfrenta desafios para se adaptar à nova família e descobrir seu passado.", "imagem": "anne_com_e.jpg"},
-    {"serie": 4, "titulo": "O verão que mudou minha vida", "genero": "Drama/Romance", "ano": "2019", "descricao": "História de romance sobre um verão que mudou a vida de uma jovem garota.", "imagem": "o_verao_que_mudou_minha_vida.jpg"},
-    {"serie": 5, "titulo": "Bridgerton", "genero": "Drama/Romance", "ano": "2022", "descricao": "Uma saga de histórias de romance antigas da realeza.", "imagem": "bridgerton.jpg"},
-    {"serie": 6, "titulo": "Stranger Things", "genero": "Ficção Científica/Terror", "ano": "2016", "descricao": "Mistério e terror se misturam em uma pequena cidade com eventos sobrenaturais.", "imagem": "stranger_things.jpg"},
-    {"serie": 7, "titulo": "Elite", "genero": "Drama/Romance", "ano": "2018", "descricao": "Drama colegial sobre adolescentes da elite e vários mistérios de uma pequena cidade.", "imagem": "elite.jpg"},
-    {"serie": 8, "titulo": "You", "genero": "Suspense/Mistério", "ano": "2018", "descricao": "Trama girada em torno de um homem obcecado que persegue suas vítimas.", "imagem": "you.jpg"}
-]
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+host = 'localhost'
+database = r'C:\Users\Aluno\Downloads\BANCO (3)\BANCO.FDB'
+user = 'sysdba'
+password = 'sysdba'
+
+#Conexão
+
+con = fdb.connect( host=host, database=database, user=user, password=password )
+
 
 @app.route('/')
-def index():
-    return render_template('pesquisa.serie.html', series=series_list)
+def index():  # put application's code here
+    return render_template('index.html')
 
-@app.route('/add_series', methods=['GET', 'POST'])
-def add_series():
-    if request.method == 'POST':
-        titulo = request.form['titulo']
-        genero = request.form['genero']
-        ano = request.form['ano']
-        descricao = request.form['descricao']
-        imagem = request.form['imagem']
-        serie = len(series_list) + 1
-        series_list.append({"serie": serie, "titulo": titulo, "genero": genero, "ano": ano, "descricao": descricao})
-        return render_template('list_series.html', series=series_list)
-    return render_template('add_series.html')
+@app.route('/agendamentos', methods=['POST'])
+def agendamentos():
+    nome = request.form['nome']
+    email = request.form['email']
+    telefone = request.form['telefone']
+    horario = request.form['horario']
+    observacoes = request.form['observacoes']
+
+
+    # Criando o cursor
+    cursor = con.cursor()
+
+    try:
+        cursor.execute("SELECT 1 FROM agendamento WHERE email = ? AND  horario =?", (email,horario))
+        if cursor.fetchone():  # Se existir algum registro
+            flash("Erro: Já possui agendamento.", "error")
+            return redirect(url_for('agendamentos'))
+
+        # Inserir o novo cadastro (sem capturar o ID)
+        cursor.execute("INSERT INTO agendamento (nome, email, telefone, horario, observacoes  ) VALUES (?, ?, ?, ?, ?)",
+                           (nome, email, telefone, horario, observacoes))
+        con.commit()
+    finally:
+        # Fechar o cursor manualmente, mesmo que haja erro
+        cursor.close()
+
+    flash("Agendamento cadastrado com sucesso!", "success")
+    return redirect(url_for('index'))
+    #buscar agendamentos
+    # Rota para exibir a lista de livros em um layout HTML
+
+@app.route('/abrir_tela_cadastro')
+def abrir_tela_cadastro():  # put application's code here
+    return render_template('cadastro.html')
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['password']
+
+
+    # Criando o cursor
+    cursor = con.cursor()
+
+    try:
+        # Verificar se o livro já existe
+        cursor.execute("SELECT 1 FROM veterinario WHERE nome  = ? AND email = ? AND senha = ?", (nome, email, senha,))
+        if cursor.fetchone():  # Se existir algum registro
+            flash("Erro: cadastro já realizado.", "error")
+            return redirect(url_for('novo'))
+
+        # Inserir o novo livro (sem capturar o ID)
+        cursor.execute("INSERT INTO veterinario (nome, email, senha) VALUES (?, ?, ?)",
+                       (nome, email, senha))
+        con.commit()
+    finally:
+        # Fechar o cursor manualmente, mesmo que haja erro
+        cursor.close()
+    flash("Conta cadastrada com sucesso!", "success")
+    return redirect(url_for('index'))
+
+@app.route('/abrir_tela_login')
+def abrir_tela_login():  # put application's code here
+    return render_template('login_veterinario.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    try:
+        email = 'giovana@gmail.com'
+        senha = '5555'
+
+        # Criando o cursor
+        cursor = con.cursor()
+        # Verificar se o livro já existe
+        cursor.execute("SELECT 1 FROM veterinario WHERE nome  = ? AND senha = ?", (email, senha,))
+        if cursor.fetchone():  # Se existir algum registro
+            flash("Erro: O email não existe", "error")
+            return redirect(url_for('login'))
+
+    # Inserir o novo livro (sem capturar o ID)
+        con.commit()
+    finally:
+    # Fechar o cursor manualmente, mesmo que haja erro
+        cursor.close()
+    flash("Conta cadastrada com sucesso!", "success")
+    return redirect('login')
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
